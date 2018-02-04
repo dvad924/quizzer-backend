8f6bef58ff285a71412b41dec1b48b4d6ddb75d2
Sameagol <sameagol@gmail.com>
2018-02-04 16:06:09 -0500

    This adds non-meaningful functionality for TestTable1
    I created the files for a new database table but didn't really change anything except the names from simple user to test table 1.

---
+++ Models/TestTable1.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace quizzer_backend.Models
+{
+    public class SimpleUser : BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ Repository/TestTable1Repository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data;
+using Npgsql;
+using quizzer_backend.Models;
+
+namespace quizzer_backend.Repository
+{
+    public class TestTable1Repository : IRepository<TestTable1>
+    {
+        private string connectionString;
+        public TestTable1Repository(IConfiguration configuration)
+        {
+            connectionString = "";
+        }
+        internal IDbConnection Connection
+        {
+            get
+            {
+                Console.Write(connectionString);
+                return new NpgsqlConnection("");
+            }
+        }
+        public void Add(TestTable1 item)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute("INSERT INTO TestTable1 (name,email) VALUES(@Name, @Email)", item);
+            }
+        }
+        public IEnumerable<TestTable1> FindAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<TestTable1>("SELECT * from TestTable1");
+            }
+        }
+        public TestTable1 FindByID(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<TestTable1>("SELECT * from TestTable1 where id = @Id", new { Id = id }).FirstOrDefault();
+            }
+        }
+        public void Remove(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute("DELETE FROM TestTable1 where id=@Id", new { Id = id });
+            }
+        }
+        public void Update(TestTable1 item)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Query("UPDATE TestTable1 SET name = @Name, email= @Email WHERE id = @Id", item);
+            }
+        }
+    }
+}
